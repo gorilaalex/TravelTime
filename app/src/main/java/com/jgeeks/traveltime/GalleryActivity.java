e575391cd975f11725171f97292aeec08382d41a
thevladeffect <alecutheman@gmail.com>
2015-04-03 23:58:34 +0300

    GalleryActivity progress

--- app/src/main/java/com/jgeeks/traveltime/GalleryActivity.java
+++ app/src/main/java/com/jgeeks/traveltime/GalleryActivity.java
@@ -1,24 +1,40 @@
 package com.jgeeks.traveltime;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 
 public class GalleryActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gallery_activty);
+        setContentView(R.layout.activity_gallery);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(GalleryActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gallery_activty, menu);
+        getMenuInflater().inflate(R.menu.menu_gallery, menu);
         return true;
     }
 
@@ -31,9 +47,13 @@ public class GalleryActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this,GalleryActivity.class);
+            startActivity(intent);
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
--- app/src/main/java/com/jgeeks/traveltime/TimelineActivity.java
+++ app/src/main/java/com/jgeeks/traveltime/TimelineActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 
 
 public class TimelineActivity extends Activity {
@@ -30,6 +31,8 @@ public class TimelineActivity extends Activity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, GalleryActivity.class);
+            startActivity(intent);
             return true;
         }
 
