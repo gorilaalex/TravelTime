0262d95339479be9de6aac70590989cb2addb613
Vlad Vidac <vladvidac@gmail.com>
2015-04-04 21:04:26 +0300

    GalleryActivity menu

--- app/src/main/java/com/jgeeks/traveltime/SingleViewActivity.java
+++ app/src/main/java/com/jgeeks/traveltime/SingleViewActivity.java
@@ -1,12 +1,18 @@
 package com.jgeeks.traveltime;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -16,6 +22,13 @@ public class SingleViewActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        ActionBar bar = getActionBar();
+        bar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#04BFBF")));
+        Window window = this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(this.getResources().getColor(R.color.color1));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_view);
 
