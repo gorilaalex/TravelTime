cffbd4789dce6db86d73b983fb3dcfa72b9fe53d
gorilaalex <gorilaalex@gmail.com>
2015-04-04 09:13:27 +0300

    splash screen

--- app/src/main/java/com/jgeeks/traveltime/SplashScreen.java
+++ app/src/main/java/com/jgeeks/traveltime/SplashScreen.java
@@ -7,18 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class TimelineActivity extends Activity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_timeline);
+        setContentView(R.layout.activity_splash_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_timeline, menu);
+        getMenuInflater().inflate(R.menu.menu_splash_screen, menu);
         return true;
     }
 
